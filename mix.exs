00b43e81a9641b0c25777d7f03f58fb2e8b6be81
Anthony Smith <anthony@sticksnleaves.com>
2018-06-29 12:38:13 -0400

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bamboo.EEx.Mixfile do
     [
       app: :bamboo_eex,
       description: "EEx template support for Bamboo",
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
